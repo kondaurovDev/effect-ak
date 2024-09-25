2543be3f0f152904d16c2a386fcbc1407777faf0
Aleksandr Kondaurov <kondaurov.dev@gmail.com>
2024-09-25 18:14:31 +0400

    up

--- packages/tg-bot/src/domain/message-update.ts
+++ packages/tg-bot/src/domain/message-update.ts
@@ -8,27 +8,27 @@ export class MessageUpdate
     message_id: S.Number, // Unix time
     date: S.Number,
     chat: S.suspend(() => Chat),
-    media_group_id: S.optional(S.String),
-    effect_id: S.optional(S.String),
-    text: S.optional(S.String),
-    photo: S.optional(S.suspend(() => S.Array(MessageFile))),
-    document: S.optional(S.suspend(() => MessageFile)),
-    caption: S.optional(S.String),
-    voice: S.optional(S.suspend(() => MessageFile)),
-    from: S.optional(S.suspend(() => User)),
-    message_thread_id: S.optional(S.Number),
-    reply_markup: S.optional(ReplyMarkup),
-    reply_to_message: S.optional(S.suspend((): S.Schema<MessageUpdate> => MessageUpdate)),
-    forward_from: S.optional(S.suspend((): S.Schema<MessageUpdate> => MessageUpdate)),
-    forward_origin: S.optional(S.suspend((): S.Schema<MessageUpdate> => MessageUpdate))
+    media_group_id: S.UndefinedOr(S.String),
+    effect_id: S.UndefinedOr(S.String),
+    text: S.UndefinedOr(S.String),
+    photo: S.UndefinedOr(S.suspend(() => S.Array(MessageFile))),
+    document: S.UndefinedOr(S.suspend(() => MessageFile)),
+    caption: S.UndefinedOr(S.String),
+    voice: S.UndefinedOr(S.suspend(() => MessageFile)),
+    from: S.UndefinedOr(S.suspend(() => User)),
+    message_thread_id: S.UndefinedOr(S.Number),
+    reply_markup: S.UndefinedOr(ReplyMarkup),
+    reply_to_message: S.UndefinedOr(S.suspend((): S.Schema<MessageUpdate> => MessageUpdate)),
+    forward_from: S.UndefinedOr(S.suspend((): S.Schema<MessageUpdate> => MessageUpdate)),
+    forward_origin: S.UndefinedOr(S.suspend((): S.Schema<MessageUpdate> => MessageUpdate))
   }) { }
 
 export type Chat = typeof Chat.Type
 export const Chat =
   S.Struct({
     id: S.Number,
-    username: S.optional(S.NonEmptyString),
-    first_name: S.optional(S.NonEmptyString),
+    username: S.UndefinedOr(S.NonEmptyString),
+    first_name: S.UndefinedOr(S.NonEmptyString),
     type: S.Literal("group", "private", "channel", "supergroup")
   })
 
@@ -37,8 +37,8 @@ export const MessageFile =
   S.Struct({
     file_id: S.String,
     file_unique_id: S.String,
-    file_size: S.optional(S.Number),
-    mime_type: S.optional(S.String),
-    duration: S.optional(S.Number)
+    file_size: S.UndefinedOr(S.Number),
+    mime_type: S.UndefinedOr(S.String),
+    duration: S.UndefinedOr(S.Number)
   })
 
